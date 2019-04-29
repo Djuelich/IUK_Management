9534197e316c72b296f82d855e98806ab829576f
root <root@localhost.localdomain>
2019-04-29 09:47:47 +0200

    testwindow

--- ui/userinterface.py
+++ ui/userinterface.py
@@ -1,23 +1,24 @@
 import sys
-from PyQT5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainwindow, QLabel, QWidget
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget
 from PyQt5.QtCore import QSize
 
-class Mainwindow(QMainwindow):
+class MyWindow(QMainWindow):
     def __init__(self):
         #Konstruktor von QMainwindow aufrufen
         super().__init__()
 
         #Fenstergröße und Titel einstellen
-        self.setMinimumSize(Qsize(800, 600))
+        self.setMinimumSize(QSize(800, 600))
         self.setWindowTitle('IUK Management')
 
-                #Title-Widget erzeugen und in Fenster einbetten
-                title = QLabel('IUK Management', self)
-                title.setAlignment(QTCore.QT.AlignCenter)
+        #Title-Widget erzeugen und in Fenster einbetten
+        title = QLabel('IUK Management', self)
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        self.setCentralWidget(title)
 
 #Fenster öffnen; das Programm läuft, bis das Fenster geschlossen wird
-app = QTWidget.QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 win = MyWindow()
 win.show()
 sys.exit(app.exec_())
