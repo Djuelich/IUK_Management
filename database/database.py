12c0320dc663aeccd15192561e1943c33decc6a9
Dominik Jülich <djuelich@uni-osnabrueck.de>
2019-04-29 10:59:32 +0200

    added try: function

--- database/database.py
+++ database/database.py
@@ -2,30 +2,32 @@
 import MySQLdb
 import dis
 
-# Connect
-db = MySQLdb.connect(host="db4free.net",
+try:
+    # Connect
+    db = MySQLdb.connect(host="db4free.net",
                      user="elwseg",
                      passwd="Scarred_92",
                      db="iukmanagement")
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-# Execute SQL select statement
-cursor.execute("SELECT * FROM Fahrzeug")
+    # Execute SQL select statement
+    cursor.execute("SELECT * FROM Fahrzeug")
 
-# Commit your changes if writing
-# In this case, we are only reading data
-# db.commit()
+    # Commit your changes if writing
+    # In this case, we are only reading data
+    # db.commit()
 
-# Get the number of rows in the resultset
-numrows = cursor.rowcount
+    # Get the number of rows in the resultset
+    numrows = cursor.rowcount
 
-# Get and display one row at a time
-for x in range(0, numrows):
-   row = cursor.fetchone()
-   print ("Fahrzeugtyp:", row[0], "Funkrufnummer:", row[1], "Besatzung:", row[2], "Status:", row[3], "Organisation:", row[4])
-  
+    # Get and display one row at a time
+    for x in range(0, numrows):
+        row = cursor.fetchone()
+        print ("Fahrzeugtyp:", row[0], "Funkrufnummer:", row[1], "Besatzung:", row[2], "Status:", row[3], "Organisation:", row[4])
 
-
-# Close the connection
-db.close()
+except BaseException as ex:
+    print('Fehler', ex)
+finally:
+    # Close the connection
+    db.close()
